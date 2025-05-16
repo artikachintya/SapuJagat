2804f8c3b157bbbfaf61d7e113559f1e8dc1f060
NatanaelYG <natanaelyohanesg@gmail.com>
2025-05-16 14:10:29 +0700

    Testing

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
