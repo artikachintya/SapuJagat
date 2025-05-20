9aef06dfeaf7606ea85d0041df039b15510d2001
Spnecr <fikriluhurpangestu@gmail.com>
2025-05-20 13:07:33 +0700

    UI login register on progress

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login1', function () {
+    return view('auth.login1');
+});
+
+Route::get('/register1', function () {
+    return view('auth.register1'); // nama file: register1.blade.php
+})->name('register1');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
