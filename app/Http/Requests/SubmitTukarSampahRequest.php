cbba3ff6a2d6ced63a7d4031fe1ff0b63dcc2579
maritzaeka <maritzaekarahmadhani@gmail.com>
2025-08-01 13:33:29 +0700

    allert tukar sampah tapi gatau kenapa ga keluar dari message error request

---
+++ app/Http/Requests/SubmitTukarSampahRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubmitTukarSampahRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Ubah sesuai kebutuhan autentikasi
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+     public function rules(): array
+    {
+        return [
+            'trash' => 'required|array',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'trash.required' => 'Silakan pilih jenis sampah yang ingin ditukar.',
+        ];
+    }
+}
